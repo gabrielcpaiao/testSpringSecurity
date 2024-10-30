7579b7246c46599d091a93c60b994d528c8d3ebd
Gabriel Paiao <costapaiao@hotmail.com>
2024-10-30 14:36:05 -0300

    This Repository is about studying Spring Boot Security and things like that

---
+++ src/test/java/gacpaiao/testSpringSecurity/TestSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package gacpaiao.testSpringSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
